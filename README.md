0429294ad1b6d211fb405637999c47d5572dd6ea
Sean Chen <skypemaster007@gmail.com>
2015-01-22 11:59:09 -0800

    Complete stopwatch.js and update README.md

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# stopwatch
+# Acelio Code Challenge
+
+Prompt: For this code challenge, design and code a Stopwatch, and then utilize it to design and code a Racer.
+
+## Documentation
+
+### new Stopwatch([listener, resolution])
+Creates a Stopwatch object with optional tick `listener` function and `resolution` milliseconds. See `setListener()` to get a better idea of what the `listener` parameter does.
+
+### Stopwatch.stop()
+Stops the watch and returns the elapsed time as an object. See Stopwatch.getElapsed() for the format of the time object.
+
+### Stopwatch.start()
+Starts the watch. If a listener is attached via `setListener()`, the listener is triggered each time `resolution` milliseconds has elapsed. 
+
+### Stopwatch.reset()
+Resets the watch to 0. If the watch is running it will continue to run from 0. Calling `reset()` does not trigger the listener function.
+
+### Stopwatch.restart()
+Restarts the watch from 0.
+
+### Stopwatch.getElapsed()
+Returns the total elapsed time in an object with attributes `hours`, `minutes`, `seconds`, and `milliseconds`.
+
+### Stopwatch.setElapsed(hours, minutes, seconds)
+Resets the stopwatch and elapsed time to the specific time. Does not stop the watch, so a running watch will immediately continue from the specified time.
+
+### Stopwatch.toString()
+Returns a human readable version of the elapsed time. Works while running.
+
+### Stopwatch.setListener(listenerFunction)
+Sets the listener of the stopwatch. The `listenerFunction` will be called each time `resolution` milliseconds have elapsed.
---
+++ stopwatch.js
@@ -0,0 +1,103 @@
+var Stopwatch = function(listener, resolution) {
+  this.startTime = 0;
+  this.stopTime = 0;
+  this.totalElapsed = 0; // elapsed number of ms in total
+  this.started = false;
+  this.listener = (listener !== undefined ? listener : null); // function to receive onTick events
+  this.tickResolution = (resolution !== undefined ? resolution : 500); // how long between each tick in milliseconds
+  this.tickInterval = null;
+  
+  this.onehour = 1000 * 60 * 60;
+  this.onemin  = 1000 * 60;
+  this.onesec  = 1000;
+};
+
+Stopwatch.prototype.start = function() {
+  var delegate = function(context, method) {
+    return function() {
+      return method.call(that);
+    }
+  };
+  if (!this.started) {
+    this.startTime = new Date().getTime();
+    this.stopTime = 0;
+    this.started = true;
+    this.tickInterval = setInterval(delegate(this, this.onTick), this.tickResolution);
+  }
+};
+
+Stopwatch.prototype.stop = function() {
+  if (this.started) {
+    this.stopTime = new Date().getTime();
+    this.started = false;
+    var elapsed = this.stopTime - this.startTime;
+    this.totalElapsed += elapsed;
+    if (this.tickInterval !== null)
+      clearInterval(this.tickInterval);
+  }
+  return this.getElapsed();
+};
+
+Stopwatch.prototype.reset = function() {
+  this.totalElapsed = 0;
+  this.startTime = new Date().getTime();
+  this.stopTime = this.startTime;
+};
+
+Stopwatch.prototype.restart = function() {
+  this.stop();
+  this.reset();
+  this.start();
+};
+
+Stopwatch.prototype.getElapsed = function() {
+  var elapsed = 0;
+  if (this.started)
+    elapsed = new Date().getTime() - this.startTime;
+  elapsed += this.totalElapsed;
+  
+  var hours = parseInt(elapsed / this.onehour);
+  elapsed %= this.onehour;
+  var mins = parseInt(elapsed / this.onemin);
+  elapsed %= this.onemin;
+  var secs = parseInt(elapsed / this.onesec);
+  var ms = elapsed % this.onesec;
+  
+  return {
+    hours: hours,
+    minutes: mins,
+    seconds: secs,
+    milliseconds: ms
+  }
+};
+
+Stopwatch.prototype.setElapsed = function(hours, mins, secs) {
+  this.reset();
+  this.totalElapsed = 0;
+  this.totalElapsed += hours * this.onehour;
+  this.totalElapsed += mins  * this.onemin;
+  this.totalElapsed += secs  * this.onesec;
+  this.totalElapsed = Math.max(this.totalElapsed, 0);
+};
+
+Stopwatch.prototype.toString = function() {
+  var zpad = function(n, digits) {
+    n = n.toString();
+    while (n.length < digits)
+      n = '0' + n;
+    return n;
+  }
+  var e = this.getElapsed();
+  return zpad(e.hours, 2) + ":" + zpad(e.minutes, 2) + ":" + zpad(e.seconds, 2);
+};
+
+Stopwatch.prototype.setListener = function(listener) {
+  this.listener = listener;
+};
+
+Stopwatch.prototype.onTick = function() {
+  if(this.listener !== null) {
+    this.listener(this);
+  }
+};
+
